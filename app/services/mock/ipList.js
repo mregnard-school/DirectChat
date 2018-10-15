9205b5033ff0724243c56ccdc0b63c6a9071c582
Irindul <mathieu.regnard2@gmail.com>
2018-10-15 15:29:30 -0400

    Updated mock for better JSON structure

--- app/services/mock/ipList.js
+++ app/services/mock/ipList.js
@@ -1,14 +1,25 @@
-module.exports =  [
+const date = new Date();
+
+module.exports = [
   {
-    "client-id": 1,
-    "ips": [
+    "client": {
+      "id": 1,
+      "pseudo": "billy",
+      "ips": [
         "127.0.0.1",
         //"127.0.0.1",
-    ]
-  }, {
-    "client-id": 2,
-    "ips": [
-        "127.0.0.1",
+      ]
+    },
+    "queue": [
+      {
+        "message": {
+          "id": 1,
+          "chat_id": 1,
+          "author_id": 1,
+          "date": date,
+          "content": "Hello",
+        }
+      }
     ]
   }
 ];

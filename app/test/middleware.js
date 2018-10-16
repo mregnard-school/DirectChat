61663766c46ba1a81462101b301b12f3f4e4d171
Irindul <mathieu.regnard2@gmail.com>
2018-10-15 23:17:55 -0400

    #6 Middleware chain implemented

--- app/test/middleware.js
+++ app/test/middleware.js
@@ -1,6 +1,8 @@
 const sinon = require('sinon');
 const expect = require('chai').expect;
 const NewLiner = require('../nodes/middlewares/newLineMiddleware');
+const MiddlewareChain = require('../nodes/middlewares/middlewarechain');
+
 
 describe("Middleware test", () => {
   it('should chain operations on a message', () => {
@@ -11,6 +13,20 @@ describe("Middleware test", () => {
     let message = "Hello";
     let expected = message + "\n" + "\n";
     message = newliner1.applyMiddleware(message);
+    
+    expect(message).to.equal(expected);
+  });
+  
+  it('should chain operations on a message using a chain', () => {
+    const newliner1 = new NewLiner();
+    const newliner2 = new NewLiner();
+    
+    const chain = new MiddlewareChain(newliner1);
+    chain.addMiddleware(newliner2);
+    
+    let message = "Hello";
+    let expected = message + "\n" + "\n";
+    message = chain.applyMiddlewares(message);
     
     expect(message).to.equal(expected);
   })

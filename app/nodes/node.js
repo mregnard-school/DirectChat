d0840b6bf7966d4862f17f72e24388d544d1e41c
Irindul <mathieu.regnard2@gmail.com>
2018-10-16 09:04:37 -0400

    #6 2-way Communication implemented

--- app/nodes/node.js
+++ app/nodes/node.js
@@ -1,10 +1,15 @@
 const net = require('net');
+const chain = require('./middlewares/');
+
+const handshakeInfo = 'handshake ';
+const handshakeAck = handshakeInfo + 'ack';
 
 class Node {
   constructor(client) {
     this.client = client;
     this.sockets = [];
     this.serverSocket = undefined;
+    this.middleWareChain = chain;
   }
   
   runServer(port) {
@@ -18,12 +23,21 @@ class Node {
   
   onConnection(socket) {
     this.sockets.push(socket);
-    socket.write("Hello friend " + "\n");
+    socket.setEncoding('utf8');
+  }
+  
+  parseAndSendHandShakeServerSide(socket, data) {
+    socket.client = this.parseHandshake(data);
+    socket.write(this.buildHandshakeMessage());
   }
   
   setupListeners(socket) {
     socket.on('data', (data) => {
-      this.onReceivedData(socket, data);
+      if (data.includes(handshakeInfo)) { // TODO irindul 2018-10-15 : Add boolean to ensure we handshake once
+        this.parseAndSendHandShakeServerSide(socket, data);
+      } else {
+        this.onReceivedData(socket, data);
+      }
     });
     
     socket.on('end', () => {
@@ -31,12 +45,26 @@ class Node {
     })
   }
   
+  buildHandshakeMessage() {
+    return handshakeInfo + this.client.id + ' ' + this.client.pseudo;
+  }
+  
+  parseHandshake(data) {
+    let parts = data.split(' ');
+    let id = Number(parts[1]);
+    let pseudo = parts[2];
+    return {
+      "id": id,
+      "pseudo": pseudo,
+    };
+  }
+  
   onReceivedData(socket, data) {
-    socket.write(data);
+    //socket.write(data);
   }
   
   onEndConnection(socket) {
-    this.sockets = this.sockets.filter(item => item !== socket);
+    //  this.sockets = this.sockets.filter(item => item !== socket);
   }
   
   closeServer() {
@@ -48,19 +76,61 @@ class Node {
   
   connectTo(ip, port) {
     const clientSocket = new net.Socket();
+    let self = this;
+    let promise = new Promise((resolve, reject) => {
+      clientSocket.connect(port, ip, () => {
+        this.sockets.push(clientSocket);
+        this.sendHandShakeClientSide(clientSocket);
+      });
+      
+      clientSocket.on('data', (data) => {
+        data = data.toString();
+        if (data.includes(handshakeInfo)) {
+          self.parseHandshakeClientSide(clientSocket, data);
+          resolve();
+        }
+      });
+      
+      clientSocket.on('close', function () {
+      });
+      
+      clientSocket.on('error', (error) => {
+        reject(error);
+      });
+    });
     
-    clientSocket.connect(port, ip);
+    return promise;
+  }
+  
+  parseHandshakeClientSide(socket, data) {
+    socket.client = this.parseHandshake(data);
+  }
+  
+  writeMessageTo(client, message) {
+    let content = this.applyMiddlewares(message);
+    let sockets = this.socketsAssociatedWithClient(client);
+    sockets.forEach(socket => {
+      socket.write(content); // TODO irindul 2018-10-16 : Construct message object here (date, author, content etc..)
+    });
+  }
+  
+  applyMiddlewares(message) {
+    return this.middleWareChain.applyMiddlewares(message);
+  }
+  
+  socketsAssociatedWithClient(client) {
+    let socketsAssociated = [];
+    for (let i = 0; i < this.sockets.length; i++) {
+      if (this.sockets[i].client.id === client.id) {
+        socketsAssociated.push(this.sockets[i]);
+      }
+    }
     
-    clientSocket.on('data', function(data) {
-      clientSocket.destroy(); // kill client after server's response
-    });
+    return socketsAssociated;
+  }
   
-    clientSocket.on('close', function() {
-    });
-  
-    clientSocket.on('error', (error) => {
-      throw (error);
-    });
+  sendHandShakeClientSide(socket) {
+    socket.write(this.buildHandshakeMessage());
   }
 }
 

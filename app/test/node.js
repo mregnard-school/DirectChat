b81c05b42bb314e55ee45e5475933193407ea1dd
Irindul <mathieu.regnard2@gmail.com>
2018-10-16 09:02:10 -0400

    Node tests added

--- app/test/node.js
+++ app/test/node.js
@@ -1,6 +1,5 @@
 const sinon = require('sinon');
 const expect = require('chai').expect;
-const assert = require('chai').assert;
 
 const Node = require('../nodes/node');
 const clients = require('../services/mock').clients;
@@ -35,11 +34,62 @@ describe("Node", () => {
     nodeServer.runServer(5000);
     
     const nodeClient = new Node(clients[0]);
-    nodeClient.connectTo('127.0.0.1', 5000);
+    nodeClient.connectTo('127.0.0.1', 5000).then(() => {
+          nodeServer.closeServer();
+          done();
+        }
+    )
+  });
+  
+  it("should parse handshake", (done) => {
+    const nodeServer = new Node(clients[0]);
+    let client = nodeServer.parseHandshake('handshake 1 John');
+    expect(client).to.be.deep.equal({id: 1, pseudo: "John"});
+    done();
+  });
+  
+  it("should return the socket for associated client", (done) => {
+    const nodeServer = new Node(clients[0]);
+    nodeServer.runServer(5000);
+    const nodeClient = new Node(clients[1]);
+    nodeClient.connectTo("127.0.0.1", 5000)
+        .then(() => {
+              let sockets = nodeServer.socketsAssociatedWithClient(clients[1]);
+              expect(sockets.length).to.deep.equal(1);
+              nodeServer.closeServer();
+              done();
+            }
+        );
+  });
+  
+  it("server should send a message to a client", (done) => {
+    const nodeServer = new Node(clients[0]);
+    const nodeClient = new Node(clients[1]);
+    const receiveStub = sinon.stub(nodeClient, 'onReceivedData');
     
-    setTimeout(() => {
+    nodeServer.runServer(5000);
+    
+    nodeClient.connectTo('127.0.0.1', 5000).then(() => {
+      nodeServer.writeMessageTo(clients[1], "Hello");
+      expect(receiveStub.calledOnce);
       nodeServer.closeServer();
       done();
-    }, 1000)
+    });
   });
+  
+  it("client should send a message to server", (done) => {
+    const nodeServer = new Node(clients[0]);
+    const nodeClient = new Node(clients[1]);
+    const receiveStub = sinon.stub(nodeServer, 'onReceivedData');
+    
+    nodeServer.runServer(5000);
+    
+    nodeClient.connectTo('127.0.0.1', 5000).then(() => {
+      nodeClient.writeMessageTo(clients[0], "Hello");
+      expect(receiveStub.calledOnce);
+      nodeServer.closeServer();
+      done();
+    })
+  });
+  
 });

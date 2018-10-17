a39f9fa50456d89a9b7e80765e323ca059cc9d64
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-10-17 10:39:11 -0400

    need database configuration

--- server/src/client.rs
+++ server/src/client.rs
@@ -2,5 +2,15 @@
 pub struct Client {
     pub id: Option<i32>,
     pub pseudo: String,
-    pub email: String
+    pub email: String,
+    pub friends: Vec<Client>,
+    pub ips: Vec<String>
+}
+
+impl Client {
+    //fonction ajout ami
+    //fonction suppression ami
+    //fonction create
+    //fonction update
+    //fonction delete
 }

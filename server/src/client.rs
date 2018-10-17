812e31a616f28c7c82334cc04ba87a03535aea09
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-10-17 10:27:54 -0400

    compiling, now need to define Client

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,13 +1,6 @@
-//
-// #[derive(Serialize, Deserialize)]
-// pub struct Client {
-//     pub id: Option<i32>,
-//     pub pseudo: String,
-//     pub email: String,
-//     pub ip: List<String>,
-//     pub friends: [Client]
-// }
-//
-// impl Client {
-//
-// }
+#[derive(Serialize, Deserialize)]
+pub struct Client {
+    pub id: Option<i32>,
+    pub pseudo: String,
+    pub email: String
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -7,39 +7,40 @@ extern crate rocket;
 
 use rocket_contrib::{Json, Value};
 
-// mod client;
-// use client::{Client};
+mod client;
+use self::client::{Client};
 
-// #[post("/", data = "<client>")]
-// fn create(client: Json<Client>) -> Json<Client> {
-//     client
-// }
-//
-// #[get("/")]
-// fn read() -> Json<Value> {
-//     Json(json!([
-//         "client 1",
-//         "client 2"
-//     ]))
-// }
-//
-// #[put("/<id>", data = "<client>")]
-// fn update(id: i32, client: Json<Client>) -> Json<Client> {
-//     client
-// }
-//
-// #[delete("/<id>")]
-// fn delete(id: i32) -> Json<Value> {
-//     Json(json!({"status": "ok"}))
-// }
+#[post("/", data = "<client>")]
+fn create(client: Json<Client>) -> Json<Client> {
+    client
+}
+
+#[get("/")]
+fn read() -> Json<Value> {
+    Json(json!([
+        "client 1",
+        "client 2"
+    ]))
+}
+
+#[put("/<id>", data = "<client>")]
+fn update(id: i32, client: Json<Client>) -> Json<Client> {
+    client
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<Value> {
+    Json(json!({"status": "ok"}))
+}
+
 #[get("/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 fn main() {
     rocket::ignite()
-        // .mount("/client", routes![create, update, delete])
-        // .mount("/clientes", routes![read])
+        .mount("/client", routes![create, update, delete])
+        .mount("/clientes", routes![read])
         .mount("/hello", routes![hello])
         .launch();
 }

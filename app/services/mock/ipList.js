6e8aecb3f77934019e73a7905af54e57af893e02
Irindul <mathieu.regnard2@gmail.com>
2018-10-18 12:00:15 -0400

    Change mock structure

--- app/services/mock/ipList.js
+++ app/services/mock/ipList.js
@@ -1,34 +1,23 @@
 const date = new Date();
 
-module.exports = [
-  {
-    "client": {
-      "id": 1,
-      "pseudo": "billy",
-      "ips": [
-        "127.0.0.1",
-        //"127.0.0.1",
-      ]
-    },
-    "queue": [
-      {
-        "message": {
-          "id": 1,
-          "chat_id": 1,
-          "author_id": 1,
-          "date": date,
-          "content": "Hello",
-        }
+
+module.exports = {
+  "friends": [{
+    "id": 2,
+    "pseudo": "billy",
+    "ips": [
+        "127.0.0.1:5001"
+    ],
+  }],
+  "queue": [
+    {
+      "message": {
+        "id": 1,
+        "chat_id": 1,
+        "author_id": 1,
+        "date": date,
+        "content": "Hello",
       },
-      {
-        "message": {
-          "id": 2,
-          "chat_id": 1,
-          "author_id": 1,
-          "date": date,
-          "content": "Hello 2",
-        }
-      }
-    ]
-  }
-];
+    }
+  ]
+};

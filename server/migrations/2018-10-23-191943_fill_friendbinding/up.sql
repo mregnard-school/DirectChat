46a4f2b09f4a16a03be08068d548f165a5c08cb9
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-10-23 16:33:45 -0400

    add new tables

---
+++ server/migrations/2018-10-23-191943_fill_friendbinding/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+-- Your SQL goes here
+INSERT INTO clients VALUES
+(1, 1, 2),
+(2, 2, 1),
+(3, 3, 4),
+(4, 4, 3),
---
+++ server/src/friend_binding.rs
@@ -0,0 +1,35 @@
+use gotham::handler::IntoResponse;
+use gotham::state::State;
+use hyper::Response;
+use gotham::http::response::create_response;
+use hyper::StatusCode;
+
+#[derive(Queryable, Serialize)]
+pub struct FriendBinding {
+    pub id: i32,
+    pub client_id: i32,
+    pub friend_id: i32,
+}
+
+/// Implements `gotham::handler::IntoResponse` trait for `Product`
+///
+/// `IntoResponse` represents a type which can be converted to a response. This trait is used in
+/// converting the return type of a function into a response.
+///
+/// This trait implementation uses the Serde project when generating responses. You don't need to
+/// know about Serde in order to understand the response that is being created here but if you're
+/// interested you can learn more at `http://serde.rs`.
+impl IntoResponse for FriendBinding {
+    fn into_response(self, state: &State) -> Response {
+        create_response(
+            state,
+            StatusCode::Ok,
+            Some((
+                serde_json::to_string(&self)
+                    .expect("serialized product")
+                    .into_bytes(),
+                mime::APPLICATION_JSON,
+            )),
+        )
+    }
+}

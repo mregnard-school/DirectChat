6fbf95e9e90531d443c8e93c7929b149a7ccfe13
Irindul <mathieu.regnard2@gmail.com>
2018-11-08 13:35:56 -0500

    Should be working when backend is updated

--- views/vue-frontend/src/renderer/components/FriendList.vue
+++ views/vue-frontend/src/renderer/components/FriendList.vue
@@ -1,23 +1,60 @@
 <template>
   <div class="friends">
-    <p>Friends :</p>
-    <div v-for="friend in friends" class="friend-container">
-      <div class="friend-info">
-        {{friend.pseudo}}
-      </div>
-      <div v-if="friend.isConnected" class="connected">
-        <div class="icon-container">
-          <span class="connected-icon"></span>
+    <div class="added">
+      <p>Friends :</p>
+      <div v-for="friend in friends" class="friend-container">
+        <div class="friend-info">
+          {{friend.pseudo}}
+        </div>
+        <div v-if="friend.isConnected" class="connected">
+          <div class="icon-container">
+            <span class="connected-icon"></span>
+          </div>
         </div>
       </div>
     </div>
+    <div class="pending">
+    </div>
+
+    <div>
+      <button class="fab" id="show-modal" @click="showModal = true">+</button>
+    </div>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <div slot="header">
+        <h3>Enter your friend's pseudo : </h3>
+      </div>
+
+
+      <div slot="body" class="add-input">
+        <label for="adder">Pseudo</label>
+        <input type="text" v-model.trim="addFriend" id="adder" placeholder="Pseudo..." autofocus>
+      </div>
+
+      <div slot="footer">
+        <button class="btn" @click.stop="handleAddNewFriend">
+          OK
+        </button>
+      </div>
+    </modal>
   </div>
 
 </template>
 
 <script>
+  import Modal from 'components/Modal';
+  import {http} from '@/axios-wrapper';
   export default {
     name: "FriendList",
+    components: {
+      Modal,
+    },
+    data() {
+      return {
+        showModal: false,
+        addFriend: '',
+      }
+    },
     computed: {
       connected() {
         return this.$store.state.Friends.connected;
@@ -27,8 +64,28 @@
       },
       friends() {
         return this.connected.concat(this.disconnected);
+      },
+      clientId() {
+        return this.state.peer.client.id;
       }
     },
+    methods: {
+      handleAddNewFriend() {
+        if(this.addFriend !== '') {
+          console.log('clicked');
+          // TODO irindul 2018-11-08 : Test when backend has been updated
+          http.post(`/clients/${this.clientId}/friends`, {
+            pseudo: this.addFriend,
+          }).then((response) => {
+            //todo push to store new pending request !
+          }).catch((error) => {
+            console.log(error);
+          });
+
+          this.showModal = false;
+        }
+      },
+    }
   }
 </script>
 
@@ -36,29 +93,104 @@
   @import '~styles/global';
 
   .friends {
-    display: flex;
-    padding-left: 10px;
-    flex-direction: column;
-    .friend-container {
+    .added {
       display: flex;
-      flex-direction: row;
-      .friend-info {
-        flex: 2;
-      }
+      padding-left: 10px;
+      flex-direction: column;
+      .friend-container {
+        display: flex;
+        flex-direction: row;
+        .friend-info {
+          flex: 2;
+        }
 
-      .connected {
-        flex: 1;
+        .connected {
+          flex: 1;
 
-        .icon-container {
-          .connected-icon {
-            height: 10px;
-            width: 10px;
-            background-color: lawngreen;
-            border-radius: 50%;
-            display: inline-block;
+          .icon-container {
+            .connected-icon {
+              height: 10px;
+              width: 10px;
+              background-color: lawngreen;
+              border-radius: 50%;
+              display: inline-block;
+            }
           }
         }
       }
+    }
+    .fab {
+      position: absolute;
+      bottom: 2%;
+      right: 3%;
+      border-radius: 100px;
+      height: 30px;
+      width: 30px;
+      font-size: 20px;
+    }
+
+    .modal-mask {
+      position: fixed;
+      z-index: 9998;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, .5);
+      display: table;
+      transition: opacity .3s ease;
+    }
+
+    .modal-wrapper {
+      display: table-cell;
+      vertical-align: middle;
+    }
+
+    .modal-container {
+      width: 300px;
+      margin: 0px auto;
+      padding: 20px 30px;
+      background-color: #fff;
+      border-radius: 2px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+      transition: all .3s ease;
+      font-family: Helvetica, Arial, sans-serif;
+    }
+
+    .modal-header h3 {
+      margin-top: 0;
+      color: #42b983;
+    }
+
+    .modal-body {
+      margin: 20px 0;
+    }
+
+    .modal-default-button {
+      float: right;
+    }
+
+    /*
+     * The following styles are auto-applied to elements with
+     * transition="modal" when their visibility is toggled
+     * by Vue.js.
+     *
+     * You can easily play with the modal transition by editing
+     * these styles.
+     */
+
+    .modal-enter {
+      opacity: 0;
+    }
+
+    .modal-leave-active {
+      opacity: 0;
+    }
+
+    .modal-enter .modal-container,
+    .modal-leave-active .modal-container {
+      -webkit-transform: scale(1.1);
+      transform: scale(1.1);
     }
   }
 </style>
